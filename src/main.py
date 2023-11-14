1ed7fccf04aa4f9e2f68771ee2122cccf31685af
Luigi311 <git@luigi311.com>
2023-11-13 21:29:13 -0700

    Move functison to utils

--- src/main.py
+++ src/main.py
@@ -3,22 +3,9 @@ import os, yaml, json, traceback, argparse
 from dotenv import load_dotenv
 from difflib import get_close_matches
 
+from src.utils import template_json_to_list, template_json_to_dict
+
 from src.unraid_templates import Unraid
-
-def template_json_to_list(variables_json: json) -> list:
-    output_list = []
-    for container_variable in variables_json:
-        output_list.append(f"{variables_json[container_variable]['Default']}:{container_variable}")
-
-    return output_list
-
-
-def template_json_to_dict(variables_json: json) -> dict:
-    output_dict = {}
-    for variable in variables_json:
-        output_dict[variable] = variables_json[variable]["Default"]
-
-    return output_dict
 
 
 def create_app_docker_compose(folder: str, app_name: str, template: json):
